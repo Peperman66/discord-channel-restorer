0dfe5be7f857f01c67d9e95f544ed3175b1139b0
Peperman66 <peperman66@seznam.cz>
2022-02-03 16:54:59 +0100

    add index.js with basic command handling

---
+++ index.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+const Discord = require("discord.js");
+const client = new Discord.Client({
+	presence: {
+		activities: [{name: 'those messages'}], status: 'watching'
+	},
+	intents: Discord.Intents.FLAGS.GUILD_MESSAGES
+});
+const fs = require('fs');
+
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith(".js"));
+for (file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.data.name, command);
+}
+
+
+client.on('ready', () => {
+	console.log("Bot is ready!");
+})
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+	console.log(interaction);
+
+	const command = client.commands.get(interaction.commandName);
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({content: "There was an error while executing this command!", ephemeral: true})
+	}
+});
+
+client.login(process.env.TOKEN);
