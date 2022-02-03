72bb7753e59ab93d5f7259004c13a675ccf82615
Peperman66 <peperman66@seznam.cz>
2022-02-03 16:55:11 +0100

    add subcommand handling to channel.js

--- commands/channel.js
+++ commands/channel.js
@@ -1,3 +1,21 @@
+const fs = require('fs');
+const Discord = require('discord.js');
+
+const subcommands = new Discord.Collection();
+const subcommandFiles = fs.readdirSync('./channel/').filter(file => file.endsWith('js'));
+for (file of subcommandFiles) {
+	const subcommand = require(`./channel/${file}`);
+	subcommands.set(subcommand.data.name, subcommand);
+}
+
+module.exports.execute = async function(interaction) {
+	const subcommand = subcommands.get(interaction.options.getSubcommand());
+	if (!subcommand) return;
+
+	await subcommand.execute(interaction);
+
+}
+
 module.exports.data = {
 	name: 'channel',
 	description: 'Manage saving and restoring channels',
