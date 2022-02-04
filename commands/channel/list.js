18883a94a57050ccd605f91b5790de2187866faf
Peperman66 <peperman66@seznam.cz>
2022-02-04 01:45:30 +0100

    add list command logic

--- commands/channel/list.js
+++ commands/channel/list.js
@@ -1,3 +1,24 @@
+const {Embed} = require('@discordjs/builders');
+const db = require('better-sqlite3')('db/data.db', {readonly: true});
+
+const statement = db.prepare(`SELECT ID, Name, LastSaved FROM Channel WHERE GuildID = ?`);
+
+module.exports.execute = async function(interaction) {
+	const result = statement.all(interaction.guildId);
+	let description = "<Channel name>\t<Channel ID>\t<Last saved>";
+	for (row of result) {
+		description += `${row.Name}\t${row.ID}\t<t:${row.LastSaved}>`;
+	}
+	if (!interaction.guild.available) {
+		await interaction.guild.fetch();
+	}
+	const embed = new Embed()
+		.setTitle(`A list of all channel saves for the guild ${interaction.guild.name}`)
+		.setThumbnail(interaction.guild.iconURL({size:4096}))
+		.setDescription(description);
+	interaction.reply({embeds:[embed]});
+}
+
 module.exports.data = {
 	name: 'list',
 	description: 'Lists all saved channels and the dates of each channel save'
