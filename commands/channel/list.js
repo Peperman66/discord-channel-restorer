19a525aefdbd7922429632f8b9764de6a1bad3f9
Peperman66 <peperman66@seznam.cz>
2022-02-04 14:15:52 +0100

    add newline to list command

--- commands/channel/list.js
+++ commands/channel/list.js
@@ -5,9 +5,9 @@ const statement = db.prepare(`SELECT ID, Name, LastSaved FROM Channel WHERE Guil
 
 module.exports.execute = async function(interaction) {
 	const result = statement.all(interaction.guildId);
-	let description = "<Channel name>\t<Channel ID>\t<Last saved>";
+	let description = "<Channel name>\t<Channel ID>\t<Last saved>\n";
 	for (row of result) {
-		description += `${row.Name}\t${row.ID}\t<t:${row.LastSaved}>`;
+		description += `${row.Name}\t${row.ID}\t<t:${row.LastSaved}>\n`;
 	}
 	if (!interaction.guild.available) {
 		await interaction.guild.fetch();
