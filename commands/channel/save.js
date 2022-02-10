9e5c530ae4ed1a5cf89406867e3b006c017553db
Peperman66 <peperman66@seznam.cz>
2022-02-11 00:12:50 +0100

    add displaying number of found messages in save

--- commands/channel/save.js
+++ commands/channel/save.js
@@ -7,25 +7,28 @@ const channelSaveExistsQuery = db.prepare('SELECT * FROM Channel WHERE ID = ?');
 const channelSaveCreateQuery = db.prepare('INSERT INTO Channel (ID, GuildID, Name, Topic, LastSaved) VALUES (?, ?, ?, ?, ?)');
 const channelSaveUpdateQuery = db.prepare('UPDATE Channel SET LastSaved = ? WHERE ID = ?');
 
-async function getAllMessagesInChannel(channel) {
+async function getAllMessagesInChannel(channel, callback) {
 	let last = undefined;
 	let messages = new Collection();
+	let interval = setInterval(() => callback(messages.size), 5000);
 	do {
 		var currentMessages = await channel.messages.fetch({before: last?.id});
 		last = currentMessages?.last();
 		messages = messages.concat(currentMessages);
-	} while (currentMessages.size > 0); 
+		console.log(messages.size);
+	} while (currentMessages.size > 0);
+	clearInterval(interval); 
 	return messages;
 }
 
 module.exports.execute = async function(interaction) {
-	await interaction.deferReply();
+	let reply = await interaction.deferReply();
 	const targetChannel = interaction.options.getChannel("channel");
 	if (targetChannel.partial) {
 		await targetChannel.fetch();
 	}
-	const messages = await getAllMessagesInChannel(targetChannel);
-	interaction.followUp(`Saving ${messages.size} messages!`);
+	const messages = await getAllMessagesInChannel(targetChannel, (messageCount) => {interaction.editReply(`Found ${messageCount} messages so far!`)});
+	interaction.editReply(`Saving ${messages.size} messages!`);
 	messageSave.save(messages)
 		.then(() => {
 			if (channelSaveExistsQuery.get(targetChannel.id) != null) {
